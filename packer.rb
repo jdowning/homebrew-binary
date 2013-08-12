93fad5ea5ab265dfced5f56449273e41b9652efd
Justin Downing <justin@apartmenttherapy.com>
2013-08-12 17:33:08 -0400

    packer 0.3.0

--- packer.rb
+++ packer.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Packer < Formula
   homepage 'http://www.packer.io'
-  version '0.2.2'
+  version '0.3.0'
 
   if Hardware.is_64_bit?
-    url 'https://dl.bintray.com/mitchellh/packer/0.2.2_darwin_amd64.zip'
-    sha1 '05cc32db261fe80f1b342850ade2c35f85613936'
+    url 'https://dl.bintray.com/mitchellh/packer/0.3.0_darwin_amd64.zip'
+    sha1 'eae4e660632ffcfcbc297a4becb75b49c31ecc7b'
   else
-    url 'https://dl.bintray.com/mitchellh/packer/0.2.2_darwin_386.zip'
-    sha1 'c0d55ed17ae97f838c13059dccfdf87d9f2bf6d8'
+    url 'https://dl.bintray.com/mitchellh/packer/0.3.0_darwin_386.zip'
+    sha1 'dcbe575176157db281019d9ec3b89c9157bbcdc1'
   end
 
   def install
