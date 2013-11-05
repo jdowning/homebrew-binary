68736425783ee3aace4dfeb33f1f02f691241586
Brendan Tobolaski <Brendan@Tobolaski.com>
2013-11-04 19:29:27 -0600

    Packer 0.3.11
    Closes #27.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- packer.rb
+++ packer.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Packer < Formula
   homepage 'http://www.packer.io'
-  version '0.3.10'
+  version '0.3.11'
 
   if Hardware.is_64_bit?
-    url 'https://dl.bintray.com/mitchellh/packer/0.3.10_darwin_amd64.zip'
-    sha256 '233d5e0ae73d6cc5955b719bcb4fa9067494659d5416bab7ac4f12d1d29c5897'
+    url 'https://dl.bintray.com/mitchellh/packer/0.3.11_darwin_amd64.zip'
+    sha256 'fc226df5dbfb189e8a1e948a2e9c12d2918001374dd3833088dd5eedd0b6fcf1'
   else
-    url 'https://dl.bintray.com/mitchellh/packer/0.3.10_darwin_386.zip'
-    sha256 '322cbf6ca112eb3b319eb6a13ce9d235ee71a1d05e1454ba6da8fc61b27f4a56'
+    url 'https://dl.bintray.com/mitchellh/packer/0.3.11_darwin_386.zip'
+    sha256 '12dc9435ef062757dd0a90b1bad2fe06e7e72378d8f33275c3143ab7cd320770'
   end
 
   def install
